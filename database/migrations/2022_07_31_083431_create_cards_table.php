f8b95ca47df292406ede33194ecde0e750ff1f65
unknown <samanmhmd73@gmail.com>
2022-07-31 17:52:46 +0430

    Make migrations and models

--- database/migrations/2022_07_31_083431_create_cards_table.php
+++ database/migrations/2022_07_31_083431_create_cards_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('card_number')->unique();
+            $table->char('pin',4);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('cards');
     }
 };
