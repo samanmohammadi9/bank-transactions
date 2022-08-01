7d7d902c1814cd39593c0d64e7a789d3802845a0
unknown <samanmhmd73@gmail.com>
2022-08-01 18:43:25 +0430

    seed database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone')->unique();
             $table->string('national_code');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
