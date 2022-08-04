f63dae1220aa36b51e9f3289d537031559dcf8b7
unknown <samanmhmd73@gmail.com>
2022-08-04 13:12:41 +0430

    get sms text data from yaml file

--- app/Http/Classes/KavehSMSSender.php
+++ app/Http/Classes/KavehSMSSender.php
@@ -3,6 +3,7 @@
 namespace App\Http\Classes;
 
 use http\Env;
+use Symfony\Component\Yaml\Yaml;
 
 class KavehSMSSender extends NotifSender
 {
@@ -10,11 +11,12 @@ class KavehSMSSender extends NotifSender
     public function send_notification($origin , $destination , $amount)
     {
         $curl = curl_init();
-        $origin_text="تست برداشت مبلغ ".$amount;
-        $origin_text="تست واریز مبلغ ".$amount;
+        $sms_file=Yaml::parse(file_get_contents('../sms.yml'));
+        $origin_text=$sms_file['origin']['text1'].$amount.$sms_file['origin']['text2'];
+
         $curl_url = 'https://api.kavenegar.com/v1/'.
             Env("KAVEH_API_KEY").'/sms/send.json?receptor='.$origin.'&message='.$origin_text.'&sender=10008663';
-        $rsp=curl_setopt_array($curl, array(
+        curl_setopt_array($curl, array(
             CURLOPT_URL => $curl_url,
             CURLOPT_HTTPHEADER => array(
                 'Authorization: Basic OlpQRkVTRzVMOTc=',
@@ -22,6 +24,16 @@ class KavehSMSSender extends NotifSender
             ),
         ));
 
+        $destination_text=$sms_file['destination']['text1'].$amount.$sms_file['destination']['text2'];
+        $curl_url = 'https://api.kavenegar.com/v1/'.
+            Env("KAVEH_API_KEY").'/sms/send.json?receptor='.$destination.'&message='.$destination_text.'&sender=10008663';
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $curl_url,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic OlpQRkVTRzVMOTc=',
+                'Content-Type: application/json'
+            ),
+        ));
         $response = curl_exec($curl);
         curl_close($curl);
 
